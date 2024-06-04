dac2354682717a5e307714dd6f566263dc9b052b
Munchicken <hacknsnack@yahoo.com>
2024-06-04 18:24:36 -0500

    added argument handler

--- ssdz.sh
+++ ssdz.sh
@@ -11,5 +11,23 @@ information() {
   echo "ssdz - Tool to zap multiple sequential disks (clear MBR & GPT)"
 }
 
+# Function to handle arguments
+handle_args() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h | --help)
+        #information
+        echo "help"
+        exit 0
+        ;;
+      *)
+        echo "default"
+        exit 0
+        ;;
+    esac
+    shift
+done
+}
+
 # Main
-information
+handle_args "$@"
