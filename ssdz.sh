e984da24f7355a263155187be25661878fc5a31e
Munchicken <hacknsnack@yahoo.com>
2024-06-04 17:06:52 -0500

    initial commit

---
+++ ssdz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#  ssdz - Sarah's Sequential Disk Zapper
+#  by Sarah Pierce
+#  
+#  Tool to zap multiple sequential disks (clear MBR & GPT)
+#  Useful for preparing a large amount of disks for zfs use.
+
+echo "ssdz - Tool to zap multiple sequential disks (clear MBR & GPT)"
